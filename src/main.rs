0fcce9c299175596bb8bbb82bce0f9f9335c5187
Jakub Pastuszek <jpastuszek@gmail.com>
2018-10-31 10:25:21 +0000

    caching DB to known location

--- src/main.rs
+++ src/main.rs
@@ -12,16 +12,21 @@ extern crate serde_derive;
 extern crate serde;
 extern crate bincode;
 extern crate itertools;
+extern crate app_dirs;
 
 use cotton::prelude::*;
 use std::io;
 use std::fs::File;
 use ipnet::*;
 use std::net::Ipv4Addr;
+use std::path::PathBuf;
 use std::str::FromStr;
 use superslice::Ext;
 use bincode::{serialize_into, deserialize_from};
 use itertools::Itertools;
+use app_dirs::*;
+
+const APP_INFO: AppInfo = AppInfo{name: "asn_tools", author: "Jakub Pastuszek"};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct AnsRecord {
@@ -38,14 +43,14 @@ impl AnsRecord {
     }
 }
 
-fn load_db<'d, R: io::Read>(data: &'d mut csv::Reader<R>) -> impl Iterator<Item=AnsRecord> + 'd {
+// https://iptoasn.com/
+fn load_ip_to_asn_csv<'d, R: io::Read>(data: &'d mut csv::Reader<R>) -> impl Iterator<Item=AnsRecord> + 'd {
     data.records().or_failed_to("read IP2ANS record")
         .filter(|record| {
             let owner = &record[4];
             !(owner == "Not routed" || owner == "None")
         })
         .flat_map(|record| {
-            //println!("{:?}", record);
             let range_start: Ipv4Addr = record[0].parse().or_failed_to("parse IP");
             let range_end: Ipv4Addr = record[1].parse().or_failed_to("parse IP");
             let as_number: u32 = record[2].parse().or_failed_to("parse AS number");
@@ -64,6 +69,34 @@ fn load_db<'d, R: io::Read>(data: &'d mut csv::Reader<R>) -> impl Iterator<Item=
         })
 }
 
+fn db_file_path() -> Result<PathBuf, Problem> {
+    let mut db_file_path = app_dir(AppDataType::UserCache, &APP_INFO, "asn_records")?;
+    db_file_path.push("db.bincode");
+    Ok(db_file_path)
+}
+
+fn cache_db(records: &[AnsRecord]) -> Result<(), Problem> {
+    let db_file_path = db_file_path()?;
+    info!("Caching DB to: {}", db_file_path.display());
+
+    let db_file = File::create(&db_file_path).problem_while_with(|| format!("creating DB file: {}", db_file_path.display()))?;
+    serialize_into(BufWriter::new(db_file), records).problem_while("serilizing DB to bincode")?;
+
+    Ok(())
+}
+
+fn load_cached_db() -> Result<Option<Vec<AnsRecord>>, Problem> {
+    let db_file_path = db_file_path()?;
+
+    if db_file_path.exists() {
+        info!("Loading cached DB from: {}", db_file_path.display());
+        let db_file = File::open(&db_file_path)?;
+        Ok(Some(deserialize_from(BufReader::new(db_file)).problem_while("read bincode DB file")?))
+    } else {
+        Ok(None)
+    }
+}
+
 #[derive(Debug, StructOpt)]
 struct Cli {
     #[structopt(flatten)]
@@ -73,25 +106,21 @@ struct Cli {
     ips: Vec<String>,
 }
 
-// https://iptoasn.com/
 fn main() {
     let args = Cli::from_args();
     init_logger(&args.logging, vec![module_path!()]);
-    info!("Loading DB... ");
-    let records = match File::open("db.bincode") {
-        Ok(db_file) => deserialize_from(BufReader::new(db_file)).or_failed_to("read bincode DB file"),
-        Err(_) => {
+
+    let records = match load_cached_db().or_failed_to("load cached DB") {
+        Some(records) => records,
+        None => {
+            info!("Loading DB from CSV... ");
             let mut rdr = csv::ReaderBuilder::new().delimiter(b'\t').from_reader(BufReader::new(File::open("ip2asn-v4.tsv").or_failed_to("open DB file")));
-            let mut records = load_db(&mut rdr).collect::<Vec<_>>();
-            info!("CSV DB loaded; sorting...");
+            let mut records = load_ip_to_asn_csv(&mut rdr).collect::<Vec<_>>();
             records.sort_by_key(|record| record.ip);
-            info!("writing bincode DB...");
-            let db_file = File::create("db.bincode").or_failed_to("open bincode DB for writing");
-            serialize_into(db_file, &records).or_failed_to("write bincode DB to file");
+            cache_db(&records).or_failed_to("cache records DB");
             records
         }
     };
-    info!("DB ready");
 
     let mut stdin_csv = if args.ips.is_empty() {
         Some(csv::ReaderBuilder::new()
